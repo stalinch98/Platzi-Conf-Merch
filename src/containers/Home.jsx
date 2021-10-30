c55bee6de7e9ac4f0b3e1be5d0e02b63213ac66a
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-30 00:56:54 -0500

    Products

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import initialState from '../initialState';
+import Products from '../components/Products';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
-    )
+        <Products products={initialState.products} />
+    );
 }
 
 export default Home;
