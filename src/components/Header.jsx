b27e35cc93b41ac12d9aa84e8756259070806f41
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-30 00:08:08 -0500

    Home

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Header.css';
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1 className="Header-title">PlatziConf Merch</h1>
+            <div className="Header-checkout">
+                Cheackout
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Information from '../containers/Information';
@@ -7,17 +8,19 @@ import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 
-const App = () =>{
+const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/checkout" component={Checkout}/>
-                <Route exact path="/checkout/information" component={Information}/>
-                <Route exact path="/checkout/payment" component={Payment}/>
-                <Route exact path="/checkout/success" component={Success}/>
-                <Route component={NotFound}/>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/checkout" component={Checkout} />
+                    <Route exact path="/checkout/information" component={Information} />
+                    <Route exact path="/checkout/payment" component={Payment} />
+                    <Route exact path="/checkout/success" component={Success} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
         </BrowserRouter>
     );
 }
