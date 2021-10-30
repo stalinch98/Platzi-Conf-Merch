c5a545d7731a6a4450b3e421de8f9ca4808d5167
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-30 18:50:39 -0500

    Links and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 function Header() {
     return (
         <div className="Header">
-            <h1 className="Header-title">PlatziConf Merch</h1>
+            <h1 className="Header-title">
+                <Link to="/">
+                    PlatziConf Merch
+                </Link>
+            </h1>
             <div className="Header-checkout">
-                Cheackout
+                <Link to="/checkout">
+                <i class="fas fa-shopping-cart"></i>
+                </Link>
             </div>
         </div>
     )
