bb68129a1c9cb98a376f32f8b7e5858dd6c26987
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-30 18:37:00 -0500

    Flujo de Pago

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
     return (
-        <h1>Success</h1>
-    )
+        <div className="Succes">
+            <div className="Success-content">
+                <h2>Nombre, Gracias por tu compra</h2>
+                <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+                <div className="Success-map">Google Maps</div>
+            </div>
+        </div>
+    );
 }
 
 export default Success;
