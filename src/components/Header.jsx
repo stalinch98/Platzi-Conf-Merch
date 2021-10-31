c7460d66c066fa91a3db3ce169bb36691ec7d224
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-30 19:29:58 -0500

    add to cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Header.css';
 
 function Header() {
+    const { state } = useContext(AppContext);
+    const { cart } = state;
+
     return (
         <div className="Header">
             <h1 className="Header-title">
@@ -12,8 +16,14 @@ function Header() {
             </h1>
             <div className="Header-checkout">
                 <Link to="/checkout">
-                <i class="fas fa-shopping-cart"></i>
+                    <i class="fas fa-shopping-cart"></i>
                 </Link>
+                {
+                    cart.length > 0 &&
+                    <div className="Header-alert">
+                        {cart.length}
+                    </div>
+                }
             </div>
         </div>
     )
