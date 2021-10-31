15281b682fe61e0998da4b9dafcda550f1311aa7
Stalin Chiguano <stalin_dany98@hotmail.com>
2021-10-31 01:02:44 -0500

    transform the address in lat and lng and use in google maps api

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map'
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
     const { state } = useContext(AppContext);
     const { buyer } = state;
+    const location = useGoogleAddress(buyer[0].address);
     return (
         <div className="Succes">
             <div className="Success-content">
                 <h2>{`${buyer}, Gracias por tu compra`}</h2>
                 <span>Tu pedido llegara en 3 dias a tu dirección:</span>
-                <Map />
+                <Map data={location}/>
             </div>
         </div>
     );
